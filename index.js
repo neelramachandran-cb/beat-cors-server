688fcb7803d558bfb1c5145b3c43734a3b9fe513
Neel Ramachandran <neel.ramachandran@coinbase.com>
2023-11-22 10:56:38 -0600

    proxy to coinbase pay

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const request = require("request");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var cors = require('cors')
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors());
 
-app.get('/jokes/random', (req, res) => {
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   next();
+// });
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://pay.coinbase.com",
+    changeOrigin: true,
+  })
+);
+
+app.get("/jokes/random", (req, res) => {
   request(
-    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    { url: "https://joke-api-strict-cors.appspot.com/jokes/random" },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
+        return res.status(500).json({ type: "error", message: err.message });
       }
 
       res.json(JSON.parse(body));
     }
-  )
+  );
 });
 
 const PORT = process.env.PORT || 3000;
